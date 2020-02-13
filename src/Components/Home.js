326ecacf434d674db9df3f97d9d9dad14a2ec239
jpettett <joseph.pettett@gmail.com>
2020-02-13 13:07:12 -0800

    refresh button added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,38 +5,39 @@ import { Link } from 'react-router-dom';
 let posts;
 class Home extends Component {
   // Deletes Tweet
-	deleteTweet = id => {
-		console.log('id', id);
-		axios
-			.delete(`http://localhost:4000/tweets/${id}`)
-			.then(() => console.log('Tweet Deleted'))
-			.catch(err => {
-				console.error(err);
-			});
-	};
+  deleteTweet = id => {
+    console.log('id', id);
+    axios
+      .delete(`http://localhost:4000/tweets/${id}`)
+      .then(() => console.log('Tweet Deleted'))
+      .catch(err => {
+        console.error(err);
+      });
+  };
 
-	render() {
-		let tweets = this.props.tweets;
-		if (tweets !== null) {
-			posts = tweets.map(tweet => {
-				return (
-					<div key={tweet._id}>
-						<h1>{tweet.name}</h1>
-						<p>{tweet.tweet}</p>
-						<Link to="/edit" onClick={() => this.props.setId(tweet._id)}>
-							<button>Edit</button>
-						</Link>
-						<button onClick={() => this.deleteTweet(tweet._id)}>Delete</button>
-					</div>
-				);
-			});
-		}
-		return (
-			<div>
-				<h2>Home Component</h2>
-				<div>{posts}</div>
-			</div>
-		);
-	}
+  render() {
+    let tweets = this.props.tweets;
+    if (tweets !== null) {
+      posts = tweets.map(tweet => {
+        return (
+          <div key={tweet._id}>
+            <h1>{tweet.name}</h1>
+            <p>{tweet.tweet}</p>
+            <Link to="/edit" onClick={() => this.props.setId(tweet._id)}>
+              <button>Edit</button>
+            </Link>
+            <button onClick={() => this.deleteTweet(tweet._id)}>Delete</button>
+          </div>
+        );
+      });
+    }
+    return (
+      <div>
+        <h2>Home </h2>
+        <button onClick={() => window.location.reload()}>Refresh</button>
+        <div>{posts}</div>
+      </div>
+    );
+  }
 }
 export default Home;
